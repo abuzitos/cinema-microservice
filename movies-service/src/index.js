ab3c3e28584614f1ae37892dd8bd3ebb940fa2b7
nelson abu samra rahal junior <abuzitos@gmail.com>
2020-05-03 18:46:12 -0300

    banco de dados movies preparada para importação

--- movies-service/src/index.js
+++ movies-service/src/index.js
@@ -5,5 +5,8 @@ const server = require('./server/server')
 const repository = require('./repository/repository')
 
 server.start(movies, repository, (err, app) => {
-    console.log('Just started')
+  console.log('Just started');
+  console.log(process.env.MONGO_CONNECTION);
+  console.log(process.env.DATABASE_NAME);
+  console.log(process.env.PORT);
 })
