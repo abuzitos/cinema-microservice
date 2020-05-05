68c63df5d21a0247f69e98713af20e7d1c9ff476
nelson abu samra rahal junior <abuzitos@gmail.com>
2020-05-05 15:31:28 -0300

    correção #29

--- cinema-catalog-service/src/index.test.js
+++ cinema-catalog-service/src/index.test.js
@@ -3,3 +3,7 @@ require('./config/mongodb.test').runTests()
 require('./server/server.test').runTests()
 require('./repository/repository.test').runTests()
 require('./api/cinema-catalog.test').runTests()
+
+process.on('exit', function(code) {
+    return console.log(`About to exit with code ${code}`);
+});
