c9f07f46beeeb7087e900906840e9ef97d775892
nelson abu samra rahal junior <abuzitos@gmail.com>
2020-05-05 16:26:12 -0300

    correção #35

--- cinema-catalog-service/src/index.test.js
+++ cinema-catalog-service/src/index.test.js
@@ -1,6 +1,7 @@
-require('dotenv-safe').load();
-//require('dotenv-safe').config();
+require('dotenv-safe').config();
 require('./config/mongodb.test').runTests();
 require('./server/server.test').runTests();
 require('./repository/repository.test').runTests();
 require('./api/cinema-catalog.test').runTests();
+
+process.exit(0);
